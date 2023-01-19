3aac88f2bc4381acf43b0da17ef192b19fc3abd4
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-19 18:10:55 -0400

    Atualizações

--- painel/src/componentes/menu_lateral/menu.php
+++ painel/src/componentes/menu_lateral/menu.php
@@ -31,6 +31,66 @@
       </div>
     </div>
 
+
+    <div class="row mb-1 menu-cinza">
+      <div class="col">
+        <h6>Serviços</h6>
+
+        <?php
+        if(in_array('Certidão de Nascimento - Visualizar', $ConfPermissoes)){
+        ?>
+        <a url="paginas/servicos/cn/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-solid fa-chart-line"></i> Certidão de Nascimento
+        </a>
+        <?php
+        }
+        if(in_array('Registro Geral - Visualizar', $ConfPermissoes)){
+        ?>
+        <a url="paginas/servicos/rg/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-solid fa-chart-line"></i> Registro Geral
+        </a>
+        <?php
+        }
+        if(in_array('CRAS - Visualizar', $ConfPermissoes)){
+        ?>
+        <a url="paginas/servicos/cras/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-solid fa-chart-line"></i> CRAS
+        </a>
+        <?php
+        }
+        if(in_array('CR - Visualizar', $ConfPermissoes)){
+        ?>
+        <a url="paginas/servicos/cr/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-solid fa-chart-line"></i> CR
+        </a>
+        <?php
+        }
+        if(in_array('Psicologia - Visualizar', $ConfPermissoes)){
+        ?>
+        <a url="paginas/servicos/psicologia/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-solid fa-chart-line"></i> Psicologia
+        </a>
+        <?php
+        }
+        if(in_array('Odontologia - Visualizar', $ConfPermissoes)){
+        ?>
+        <a url="paginas/servicos/odontologia/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-solid fa-chart-line"></i> Odontologia
+        </a>
+        <?php
+        }
+        if(in_array('Jurídico - Visualizar', $ConfPermissoes)){
+        ?>
+        <a url="paginas/servicos/juridico/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-solid fa-chart-line"></i> Jurídico
+        </a>
+        <?php
+        }
+        ?>
+      </div>
+    </div>
+
+
     <div class="row mb-1 menu-cinza">
       <div class="col">
         <a url="src/usuarios/index.php" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
