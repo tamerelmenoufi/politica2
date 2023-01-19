403ed945884f7e6331f80ea755b500933a6aaf75
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-19 09:20:37 -0400

    Atualizações

--- painel/src/componentes/menu_lateral/menu.php
+++ painel/src/componentes/menu_lateral/menu.php
@@ -1,5 +1,5 @@
 <?php
-    include("{$_SERVER['DOCUMENT_ROOT']}/politica/portal/painel/lib/includes.php");
+    include("{$_SERVER['DOCUMENT_ROOT']}/politica/painel/lib/includes.php");
 ?>
 
 <style>
