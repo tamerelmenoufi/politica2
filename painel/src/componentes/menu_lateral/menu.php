6d2765076eed8832eaa030d3a88335aaf9207902
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-19 22:51:37 -0400

    Atualizações

--- painel/src/componentes/menu_lateral/menu.php
+++ painel/src/componentes/menu_lateral/menu.php
@@ -39,51 +39,65 @@
         <?php
         if(in_array('Certidão de Nascimento - Visualizar', $ConfPermissoes)){
         ?>
-        <a url="paginas/servicos/cn/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-solid fa-chart-line"></i> Certidão de Nascimento
-        </a>
+        <p>
+          <a url="paginas/servicos/cn/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+            <i class="fa-solid fa-chart-line"></i> Certidão de Nascimento
+          </a>
+        </p>
         <?php
         }
         if(in_array('Registro Geral - Visualizar', $ConfPermissoes)){
         ?>
+        <p>
         <a url="paginas/servicos/rg/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-chart-line"></i> Registro Geral
         </a>
+        </p>
         <?php
         }
         if(in_array('CRAS - Visualizar', $ConfPermissoes)){
         ?>
+        <p>
         <a url="paginas/servicos/cras/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-chart-line"></i> CRAS
         </a>
+        </p>
         <?php
         }
         if(in_array('CR - Visualizar', $ConfPermissoes)){
         ?>
+        <p>
         <a url="paginas/servicos/cr/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-chart-line"></i> CR
         </a>
+        </p>
         <?php
         }
         if(in_array('Psicologia - Visualizar', $ConfPermissoes)){
         ?>
+        <p>
         <a url="paginas/servicos/psicologia/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-chart-line"></i> Psicologia
         </a>
+        </p>
         <?php
         }
         if(in_array('Odontologia - Visualizar', $ConfPermissoes)){
         ?>
+        <p>
         <a url="paginas/servicos/odontologia/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-chart-line"></i> Odontologia
         </a>
+        </p>
         <?php
         }
         if(in_array('Jurídico - Visualizar', $ConfPermissoes)){
         ?>
+        <p>
         <a url="paginas/servicos/juridico/index.php?categoria=l" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-chart-line"></i> Jurídico
         </a>
+        </p>
         <?php
         }
         ?>
