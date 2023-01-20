7261b882b1116d7544e6443de5a79719fb6ea965
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-20 10:37:13 -0400

    Atualizações

--- painel/paginas/cadastros/permissoes/visualizar.php
+++ painel/paginas/cadastros/permissoes/visualizar.php
@@ -35,17 +35,6 @@ $d = mysqli_fetch_object($result);
 
 ?>
 
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb shadow bg-gray-custom">
-            <li class="breadcrumb-item"><a href="#" url="content.php">Início</a></li>
-            <li class="breadcrumb-item" aria-current="page">
-                <a href="#" url="<?= $urlPermissoes; ?>/index.php">Permissões</a>
-            </li>
-            <li class="breadcrumb-item active" aria-current="page">
-                Visualizar
-            </li>
-        </ol>
-    </nav>
 
     <div class="card shadow m-3">
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
