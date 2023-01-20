feb99d6c85443d91c8c34c510f98dc3b417784f6
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-20 16:23:40 -0400

    Atualizações

--- painel/paginas/servicos/cr/index.php
+++ painel/paginas/servicos/cr/index.php
@@ -50,7 +50,7 @@ $query = "SELECT s.*, a.nome AS assessor, b.nome AS beneficiado, t.descricao as 
     . "LEFT JOIN beneficiados b ON b.codigo = s.beneficiado "
     . "LEFT JOIN especialidades t ON t.codigo = s.especialidade "
     . "LEFT JOIN local_fontes lf ON lf.codigo = s.local_fonte "
-    . "WHERE s.tipo = '4' AND s.deletado = '0'"
+    . "WHERE s.tipo = '4' AND s.deletado = '0' ".(($_SESSION['query_busca'])?:false)
     . "ORDER BY s.codigo DESC";
 
 $result = mysqli_query($con, $query);
