7f265ca5434f67924645edb8a704ea8b0f3e0384
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-20 14:58:04 -0400

    Atualizações

--- painel/lib/fn.php
+++ painel/lib/fn.php
@@ -281,8 +281,8 @@ function CampoBusca($l){
 ?>
     <div class="input-group mb-3">
     <span class="input-group-text" id="inputGroup-sizing-default">Filtro</span>
-    <input type="text" id="CampoBusca" local="<?=$l?>" class="form-control" aria-label="Digite o texto para a busca" aria-describedby="inputGroup-sizing-default">
-    <button class="btn btn-outline-secondary" type="button" id="botaoCampoBusca">Buscar</button>
+    <input type="text" id="CampoBusca" class="form-control" aria-label="Digite o texto para a busca" aria-describedby="inputGroup-sizing-default">
+    <button class="btn btn-outline-secondary" local="<?=$l?>" type="button" id="botaoCampoBusca">Buscar</button>
     </div>
 <?php
 }
