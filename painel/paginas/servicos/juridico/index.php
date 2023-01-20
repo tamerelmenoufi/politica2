0ef6fea236c26b98edb3a61b26b8d0165f4ff88e
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-20 10:21:08 -0400

    Atualizações

--- painel/paginas/servicos/juridico/index.php
+++ painel/paginas/servicos/juridico/index.php
@@ -47,7 +47,7 @@ $_SESSION['saude_xls'] = false;
 <div class="card shadow m-3">
     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
         <h6 class="m-0 font-weight-bold text-primary">
-            Serviços
+            Serviços - Jurídico
         </h6>
         <?php
         if(in_array('Jurídico - Cadastrar', $ConfPermissoes)){
