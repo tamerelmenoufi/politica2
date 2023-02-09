7ef431bc41b25bd8b5506fab663e5ce21f892e87
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-09 15:30:02 -0400

    Atualizações

--- painel/paginas/servicos/odontologia/form.php
+++ painel/paginas/servicos/odontologia/form.php
@@ -97,7 +97,7 @@ if ($codigo) {
     }
 </style>
 
-<div class="card shadow mb-4">
+<div class="card shadow m-3">
     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
         <h6 class="m-0 font-weight-bold text-primary">
             <?= $codigo ? 'Alterar' : 'Cadastrar'; ?> Odontologia
