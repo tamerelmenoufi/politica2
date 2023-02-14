c4eeea939ecb400396cbbd444086fb955e7d60ec
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-14 09:19:18 -0400

    Atualizações

--- painel/paginas/cadastros/assessores/novo.php
+++ painel/paginas/cadastros/assessores/novo.php
@@ -12,22 +12,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!$codigo) $data['data_cadastro'] = 'NOW()';
 
     foreach ($data as $name => $value) {
-        $attr[] = "{$name} = '" . Addslashes($value) . "'";
+        $attr[] = "{$name} = '" . addslashes($value) . "'";
     }
 
     $attr = implode(', ', $attr);
 
-    $query = "INSERT INTO assessores SET {$attr}";
+    if ($codigo) {
+        $query = "UPDATE assessores SET {$attr} WHERE codigo = '{$codigo}'";
+    } else {
+        $query = "INSERT INTO assessores SET {$attr}";
+    }
 
     if (mysqli_query($con, $query)) {
-        $codigo = mysqli_insert_id($con);
+        $codigo = $codigo ?: mysqli_insert_id($con);
 
         sis_logs('assessores', $codigo, $query);
 
         echo json_encode([
             'status' => true,
             'msg' => 'Dados salvo com sucesso',
-            'nome' => $_POST['nome'],
             'codigo' => $codigo,
         ]);
     } else {
@@ -42,20 +45,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     exit;
 }
 
-?>
-<style>
-    .btn-fechar{
-        position:absolute;
-        right:20px;
-        top:20px;
-        cursor:pointer;
-        font-size:20px;
-    }
-</style>
+$codigo = $_GET['codigo'];
 
+if ($codigo) {
+    $query = "SELECT * FROM assessores WHERE codigo = '{$codigo}'";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
+}
+
+?>
+
+<div class="card shadow m-3">
+    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+        <h6 class="m-0 font-weight-bold text-primary">
+            <?= $codigo ? 'Alterar' : 'Cadastrar'; ?> assessores
+        </h6>
+    </div>
+    <div class="card-body">
         <form id="form-assessores">
-            <h3>CADASTRO DE NOVO ASSESSOR</h3>
-            <span Fechar class="btn-fechar"><i class="fas fa-times"></i></span>
             <div class="form-group">
                 <label for="nome">Nome <i class="text-danger">*</i></label>
                 <input
@@ -156,7 +163,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
 
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <div class="form-group">
                         <label for="municipio">
                             Municipio <i class="text-danger">*</i>
@@ -184,7 +191,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                     </div>
                 </div>
-                <div class="col-md-8">
+
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="bairro">
+                            Bairro <i class="text-danger">*</i>
+                        </label>
+                        <div id="container-bairro">
+                            <select
+                                    class="form-control bairro"
+                                    id="bairro"
+                                    name="bairro"
+                                    data-live-search="true"
+                                    data-none-selected-text="Selecione"
+                                    required
+                            >
+                                <option value="">:: Selecione ::</option>
+                                <?php
+                                if ($codigo):
+                                    $query = "SELECT * FROM bairros ORDER BY descricao";
+                                    $result = mysqli_query($con, $query);
+
+                                    while ($s = mysqli_fetch_object($result)): ?>
+                                        <option
+                                            <?= ($codigo and $d->bairro == $s->codigo) ? 'selected' : ''; ?>
+                                                value="<?= $s->codigo ?>">
+                                            <?= $s->descricao; ?>
+                                        </option>
+                                    <?php
+                                    endwhile;
+                                endif;
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
                     <div class="form-group">
                         <label for="endereco">
                             Endereco <i class="text-danger">*</i>
@@ -224,35 +268,135 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <?php endforeach; ?>
 
                 </select>
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="esfera">
+                            Esfera <i class="text-danger">*</i>
+                        </label>
+                        <select
+                                class="form-control mb-2"
+                                id="esfera"
+                                name="esfera"
+                                required
+                        >
+                            <option value="">:: Selecione ::</option>
+                            <?php
+                            foreach (getEsfera() as $value): ?>
+                                <option
+                                    <?= ($codigo and $d->esfera == $value) ? 'selected' : ''; ?>
+                                        value="<?= $value; ?>">
+                                    <?= $value; ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="secretaria">
+                            Secretaria <i class="text-danger">*</i>
+                        </label>
+                        <div id="container-secretaria">
+                            <select
+                                    class="form-control secretaria"
+                                    id="secretaria"
+                                    name="secretaria"
+                                    data-live-search="true"
+                                    data-none-selected-text="Selecione"
+                                    required
+                            >
+                                <option value="">:: Selecione ::</option>
+                                <?php
+                                if ($codigo):
+                                    $query = "SELECT * FROM secretarias WHERE esfera = '{$d->esfera}' ORDER BY descricao";
+                                    $result = mysqli_query($con, $query);
+
+                                    while ($s = mysqli_fetch_object($result)): ?>
+                                        <option
+                                            <?= ($codigo and $d->secretaria == $s->codigo) ? 'selected' : ''; ?>
+                                                value="<?= $s->codigo ?>">
+                                            <?= $s->descricao; ?>
+                                        </option>
+                                    <?php
+                                    endwhile;
+                                endif;
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="indicacao">
+                            Indicação <i class="text-danger">*</i>
+                        </label>
+                        <input
+                                type="text"
+                                class="form-control mb-2"
+                                id="indicacao"
+                                name="indicacao"
+                                value=" <?= $d->indicacao; ?>"
+                                required
+                        >
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="form-group">
+                <label for="situacao">Situação <i class="text-danger">*</i></label>
+                <select
+                        class="form-control mb-2"
+                        id="situacao"
+                        name="situacao"
+                        required
+                >
+                    <option value="">:: Selecione ::</option>
+                        <option
+                            <?= '0' == $d->situacao ? "selected" : ""; ?>
+                                value="0"
+                        >
+                            Desativado
+                        </option>
+                        <option
+                            <?= '1' == $d->situacao ? "selected" : ""; ?>
+                                value="1"
+                        >
+                            Ativado
+                        </option>
+                </select>
 
             </div>
+
             <input type="hidden" id="codigo" value="<?= $codigo; ?>">
 
             <button type="submit" class="btn btn-success">Salvar</button>
         </form>
+    </div>
+</div>
 
 <script>
     $(function(){ Carregando('none');
         $("#cpf").mask("999.999.999-99");
 
-        $("#telefone").mask("(99) 9 9999-9999");
+        $('#telefone').mask('(99) 9 9999-9999');
 
-        $('#municipio').selectpicker();
+        // $('#municipio').selectpicker();
 
-        $('#form-assessores').validate();
-
-        $("span[Fechar]").click(function(){
-            $("div[NovoAssessorBG]").css("display","none");
-            $("div[NovoAssessor]").css("display","none");
-            $("div[NovoAssessor]").html('');
-            $("#assessor").val('');
-            $("#assessor").selectpicker('refresh');
-        });
+        // $('#form-assessores').validate();
 
         $('#form-assessores').submit(function (e) {
             e.preventDefault();
 
-            if (!$(this).valid()) return false;
+            // if (!$(this).valid()) return false;
 
             var codigo = $('#codigo').val();
             var dados = $(this).serializeArray();
@@ -271,21 +415,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     if (retorno.status) {
                         $.alert(retorno.msg);
 
-
-                        $("#assessor").append('<option value="'+retorno.codigo+'">'+retorno.nome+'</option>');
-                        $("#assessor").selectpicker('refresh');
-                        $("#assessor").selectpicker('val', retorno.codigo);
-
-                        $("div[NovoAssessorBG]").css("display","none");
-                        $("div[NovoAssessor]").css("display","none");
-                        $("div[NovoAssessor]").html('');
-
+                        $.ajax({
+                            url: '<?= $urlAssessores; ?>/visualizar.php',
+                            data: {codigo: retorno.codigo},
+                            success: function (response) {
+                                $("#paginaHome").html(response);
+                            }
+                        })
                     } else {
                         $.alert(retorno.msg);
                     }
                 }
             })
         });
+
+
+        $('#esfera').change(function () {
+            var valor = $(this).val();
+            $.ajax({
+                url: '<?= $urlAssessores; ?>/select_secretarias.php',
+                data: {esfera: valor},
+                success: function (response) {
+                    $('#container-secretaria').html(response);
+                },
+                error:function(){
+                    alert('Erro no carregamento')
+                }
+            })
+        });
+
     });
 </script>
 
