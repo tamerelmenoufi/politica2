c026aa9418054629ecc19b163a6ae8aa3c38f924
livia128924 <livia128924@gmail.com>
2023-02-15 18:20:04 -0300

    atualizando

--- painel/paginas/cadastros/acao_social_tipo/form.php
+++ painel/paginas/cadastros/acao_social_tipo/form.php
@@ -80,6 +80,7 @@ if ($codigo) {
             <input type="hidden" id="codigo" value="<?= $codigo; ?>">
 
             <button type="submit" class="btn btn-success">Salvar</button>
+            <button type="button" class="btn btn-danger voltar">Voltar</button>
         </form>
     </div>
 </div>
@@ -122,6 +123,15 @@ if ($codigo) {
                     }
                 }
             })
+        });
+    });
+    $(".voltar").click(function(){
+        $.ajax({
+            url:"<?= $servicoTipo; ?>/index.php",
+            type:"POST",
+            success:function(dados){
+                $("#paginaHome").html(dados);
+            }
         });
     });
 </script>
