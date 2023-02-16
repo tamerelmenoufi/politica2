5b9f10b785f055cd147a5df6910f0ac54e89d669
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-16 08:34:50 -0400

    Atualizações

--- lib/header.php
+++ lib/header.php
@@ -8,4 +8,4 @@ $caminho_vendor = getUrl() . "lib/vendor";
 <!--<link rel="stylesheet" href="<? /*= "{$caminho_vendor}/bootstrap/css/bootstrap.min.css"; */ ?>">-->
 <link rel="stylesheet" href="<?= "{$caminho_vendor}/startbootstrap-sb-admin-2/css/sb-admin-2.min.css"; ?>">
 <link type="text/css" rel="stylesheet" href="<?= "{$caminho_vendor}/fontawesome/css/all.min.css"; ?>">
-<link href="<?=$caminho_vendor?>/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
+<link href="painel/lib/vendor/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
